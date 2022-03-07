bb9c75b3233d164347cf41f867ee1fd842e1fd66
luastan <saulbarral@gmail.com>
2022-03-07 07:50:57 +0100

    In-line code copy updated

--- components/render/MarkdownContent.vue
+++ components/render/MarkdownContent.vue
@@ -45,6 +45,17 @@ export default {
       })
     }
 
+    // In-line codeblocks
+    const vm = this;
+    document.querySelectorAll(":not(pre) > code")
+      .forEach(el => {
+        el.classList.add('copy-me');
+        el.onclick = ({target}) => {
+          navigator.clipboard.writeText(target.textContent)
+          vm.$nuxt.$emit('toast-message', "Text copied to clipboard !");
+        };
+      });
+
     const checkElements = document.getElementsByClassName('tricks-check');
     for (let checkElement of checkElements) {
       checkElement.parentElement.parentElement.classList.add('tricks-checklist');
